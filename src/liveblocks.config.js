06dda12eb4e45ac5881e263c89aa687f979e0bff
nirav <nirav.easternts@gmail.com>
2025-01-11 12:18:30 +0530

    Update liveblocks.config.js to set the public API key for Liveblocks integration, enabling real-time collaboration features in the application.

--- src/liveblocks.config.js
+++ src/liveblocks.config.js
@@ -2,7 +2,7 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
 const client = createClient({
-  publicApiKey: "YOUR_LIVEBLOCKS_PUBLIC_KEY",
+  publicApiKey: "pk_dev_eZFmL40jLyljMCvLPjq9dp_AYG2qxhMjMFPz0OOoGLQ4AwQN6OrD_IE2qww0c6zC",
 });
 
 export const {
